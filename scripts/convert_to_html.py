105da843df46a3ed9275048dd49c4bb0531b0753
Francesco Lodolo <flodolo@mozilla.com>
2021-03-25 17:55:44 +0100

    Update data and scripts (#6)

--- scripts/convert_to_html.py
+++ scripts/convert_to_html.py
@@ -11,48 +11,52 @@ def main():
 
     # Read JSON data
     file_name = os.path.join(
-        script_folder, os.path.pardir, 'output', 'languages_curated.json')
+        script_folder, os.path.pardir, "output", "languages_curated.json"
+    )
     with open(file_name) as inputfile:
         curated_data = json.load(inputfile)
 
     # Read HTML template
-    file_name = os.path.join(
-        script_folder, os.path.pardir, 'templates', 'curated.html')
+    file_name = os.path.join(script_folder, os.path.pardir, "templates", "curated.html")
     with open(file_name) as inputfile:
         template = inputfile.read()
 
     tbl_content = []
     for locale_code, language_name in curated_data.items():
-        tbl_content.append('''
+        tbl_content.append(
+            """
         <tr>
             <td>{}</td>
             <td>{}</td>
-        </tr>'''.format(locale_code, language_name))
+        </tr>""".format(
+                locale_code, language_name
+            )
+        )
 
     # Write HTML output
-    template = template.replace('%TABLEBODY%', '\n'.join(tbl_content))
-    file_name = os.path.join(
-        script_folder, os.path.pardir, 'docs', 'index.html')
-    with open(file_name, 'w') as outputfile:
+    template = template.replace("%TABLEBODY%", "\n".join(tbl_content))
+    file_name = os.path.join(script_folder, os.path.pardir, "docs", "index.html")
+    with open(file_name, "w") as outputfile:
         outputfile.write(template)
 
     # Complete analysis list
 
     # Read JSON data
-    file_name = os.path.join(
-        script_folder, os.path.pardir, 'output', 'languages.json')
+    file_name = os.path.join(script_folder, os.path.pardir, "output", "languages.json")
     with open(file_name) as inputfile:
         complete_data = json.load(inputfile)
 
     # Read HTML template
     file_name = os.path.join(
-        script_folder, os.path.pardir, 'templates', 'complete.html')
+        script_folder, os.path.pardir, "templates", "complete.html"
+    )
     with open(file_name) as inputfile:
         template = inputfile.read()
 
     tbl_content = []
     for locale_code, locale_data in complete_data.items():
-        tbl_content.append('''
+        tbl_content.append(
+            """
         <tr>
             <td>{}</td>
             <td>{}</td>
@@ -60,21 +64,22 @@ def main():
             <td>{}</td>
             <td>{}</td>
             <td>{}</td>
-        </tr>'''.format(
-            'Yes' if locale_data['cldr-available'] else 'No',
-            locale_code,
-            locale_data['cldr-name'],
-            locale_data['transform-type'],
-            locale_data['transformed-name'],
-            locale_data['mozilla-name']))
+        </tr>""".format(
+                "Yes" if locale_data["cldr-available"] else "No",
+                locale_code,
+                locale_data["cldr-name"],
+                locale_data["transform-type"],
+                locale_data["transformed-name"],
+                locale_data["mozilla-name"],
+            )
+        )
 
     # Write HTML output
-    template = template.replace('%TABLEBODY%', '\n'.join(tbl_content))
-    file_name = os.path.join(
-        script_folder, os.path.pardir, 'docs', 'complete.html')
-    with open(file_name, 'w') as outputfile:
+    template = template.replace("%TABLEBODY%", "\n".join(tbl_content))
+    file_name = os.path.join(script_folder, os.path.pardir, "docs", "complete.html")
+    with open(file_name, "w") as outputfile:
         outputfile.write(template)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
